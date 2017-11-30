0ac9a2cec4c7efe0c4badcff81746213ee4eed0f
Aleksey Pohodnya <pohodnyaa@gmail.com>
2017-11-30 16:57:24 +0700

    Fixing working with FastBoot 1.0.*

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 /* eslint-env node */
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const Merge = require('broccoli-merge-trees');
+const fastbootTransform = require('fastboot-transform');
+const path = require('path');
+const existSync = require('exists-sync');
+
 module.exports = {
   name: 'ember-cli-bootstrap-datetimepicker',
+
+  /**
+   * Hook to read all browser specific libraries from bower and wrap them up with FastBoot check.
+   * They by default are under the vendor tree.
+   *
+   * @param {Broccoli} tree
+   */
+  treeForVendor(tree) {
+    let trees = [];
+
+    if (tree) {
+      trees.push(tree);
+    }
+
+    const assetDir = path.join(this.project.root, 'node_modules', 'eonasdan-bootstrap-datetimepicker', 'src', 'js');
+
+    if (existSync(assetDir)) {
+      const browserTrees = fastbootTransform(new Funnel(assetDir, {
+        files: ['bootstrap-datetimepicker.js'],
+        destDir: 'bootstrap-datetimepicker'
+      }));
+      trees.push(browserTrees);
+    }
+
+    return new Merge(trees);
+  },
+
 
   included: function(app) {
     this._super.included(app);
 
     // Import unminified css and js
     let basePath = 'node_modules/eonasdan-bootstrap-datetimepicker';
-    app.import(`${basePath}/build/css/bootstrap-datetimepicker.css`, {
-      using: [{
-        transformation: 'fastboot-transform'
-      }]
-    });
-    app.import(`${basePath}/src/js/bootstrap-datetimepicker.js`, {
-      using: [{
-        transformation: 'fastboot-transform'
-      }]
-    });
+    app.import(`${basePath}/build/css/bootstrap-datetimepicker.css`);
+    app.import('vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.js');
   }
 };
